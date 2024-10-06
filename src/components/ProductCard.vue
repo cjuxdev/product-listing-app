789157d407c5b7c7bc9b6b9607e7ab3a6cf62828
chathura jayaranga <cj.siriwardhana@gmail.com>
2024-10-07 02:09:36 +0530

    product card component dev

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div class="product-image">
+      <img src="@/assets/mock-data/images/women-1.jpeg" />
+    </div>
+    <div class="product-info">
+      <div class="product-title">Clara long sleeve shirt</div>
+      <div class="product-category">Wardrobe essentials</div>
+      <div class="product-price">$ 78 . 00</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
