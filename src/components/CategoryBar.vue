3b07819095138d4b55a6a2cb4926de099f7fc3bf
chathura jayaranga <cj.siriwardhana@gmail.com>
2024-10-07 02:11:17 +0530

    Category bar dev

---
+++ src/components/CategoryBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="category-list">
+    <ul>
+      <li class="active">All</li>
+      <li>Blazers</li>
+      <li>Skirts</li>
+      <li>Dresses</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
