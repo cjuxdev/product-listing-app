7b537510c558926b4f72cfcbf90cdd3f7d05ea41
chathura jayaranga <cj.siriwardhana@gmail.com>
2024-10-07 02:10:56 +0530

    Pagination bar dev

---
+++ src/components/PaginationBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pagination-bar">
+    <button class="btn-right">
+      <img src="@/assets/img/right.png" />
+    </button>
+    <div class="page-numbers-container">
+      <div class="page-number">1</div>
+      <div class="page-number">2</div>
+      <div class="page-number">3</div>
+      <div class="page-number">4</div>
+      <div class="page-number">5</div>
+    </div>
+    <button class="btn-left">
+      <img src="@/assets/img/left.png" />
+    </button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
