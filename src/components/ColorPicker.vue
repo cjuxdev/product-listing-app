26d018b59d76a2db31a41a1a13d69536b767e9ee
chathura jayaranga <cj.siriwardhana@gmail.com>
2024-10-09 21:17:49 +0530

    color picker dev

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="color-picker">
+    <div
+      v-for="(color, index) in colors"
+      :key="index"
+      class="color-box"
+      @click="selectColor(color)"
+      :class="{ selected: selectedColor === color }"
+    >
+      <div class="innner" :style="{ backgroundColor: color }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    colors: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      selectedColor: null,
+    };
+  },
+  methods: {
+    selectColor(color) {
+      this.selectedColor = color;
+      this.$emit("color-selected", color);
+    },
+  },
+};
+</script>
